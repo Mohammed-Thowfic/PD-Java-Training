7b4458f6e5927e18d5febb3f5c3684a7fc1b8f42
m.syed <mdthowfic485@gmail.com>
2026-01-02 15:41:23 +0530

    Odd_Even program

---
+++ Odd_Even.java
@@ -0,0 +1,11 @@
+public class Odd_Even {
+    public static void main(String[] args) {
+        int a=2;
+        if (a%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
