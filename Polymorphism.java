ff93daadeeec53405e01bca2026738aa45a7fd53
Mohammed-Thowfic <137506737+Mohammed-Thowfic@users.noreply.github.com>
2026-01-06 10:20:57 +0530

    Polymorphism.java

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,31 +1,27 @@
-class Employee1{
-    void display() {
-        System.out.println("Method Overloading");
+class Animal {
+    void sound() {
+        System.out.println("Animal sound");
     }
 
-    void display(int id, String name) {
-        System.out.println(id + " " + name);
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog is barking");
     }
 }
-class Details extends Employee1{
-    @Override
-    public void display() {
-        System.out.println("Method Overriding");
-    }
 
-    @Override
-    public void display(int id, String  name) {
-        System.out.println(id+" "+name);
+class Cat extends Animal {
+    void sound() {
+        System.out.println("Cat is Meow");
     }
 }
 
 public class Polymorphism {
     public static void main(String[] args) {
-        Employee1 m=new Employee1();
-        m.display();
-        m.display(1,"Thowfic");
-        Details c=new Details();
-        c.display();
-        c.display(2,"Mohammed");
+        Animal cat = new Cat();
+        cat.sound();
+        Animal dog = new Dog();
+        dog.sound();
     }
 }
